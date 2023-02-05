efa61cb0dbe31e243a3c6752563c601d3fff34da
MigVC <86692813+MigVC@users.noreply.github.com>
2023-02-04 20:12:11 -0500

    fix

--- src/pages/Principal/Registro/index.jsx
+++ src/pages/Principal/Registro/index.jsx
@@ -5,12 +5,12 @@ export const InfoRegistro = () => {
     return (
         <div className='d-flex justify-content-center px-5'>
             <div className='row d-flex align-items-center '>
-                <div className='col-lg-6 col-sm-12 px-4 py-3 '>
+                <div className='col-lg-5 col-sm-12 px-4 py-3 '>
                     <p className='fs-4' style={{"fontFamily":"montserrat"}}>
                         Si eres dueño de una cancha, puedes registrar tu negocio en nuestra página completamente gratis.
                     </p>
                 </div>
-                <div className='col-lg-6 col-sm-12 px-4 py-3 '>
+                <div className='col-lg-7 col-sm-12 px-4 py-3 '>
                     <FormCanchero />
                 </div>
             </div>
